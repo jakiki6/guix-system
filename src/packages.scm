7467bdd4cb24f2673460c2f93b76aeea3ee7a4d6
Jakob Kirsch <jakob.kirsch@web.de>
2023-10-27 17:57:01 +0200

    e

--- src/packages.scm
+++ src/packages.scm
@@ -87,6 +87,89 @@
       "Off-grid, resilient mesh communication with strong encryption, forward secrecy and extreme privacy.")
     (license license:gpl3)))
 
+(define-public rust-thiserror-impl-1
+  (package
+    (name "rust-thiserror-impl")
+    (version "1.0.50")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "thiserror-impl" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "1f0lmam4765sfnwr4b1n00y14vxh10g0311mkk0adr80pi02wsr6"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:skip-build?
+        #t
+        #:cargo-inputs
+        (("rust-proc-macro2" ,rust-proc-macro2-1)
+         ("rust-quote" ,rust-quote-1)
+         ("rust-syn" ,rust-syn-2))))
+    (home-page
+      "https://github.com/dtolnay/thiserror")
+    (synopsis
+      "Implementation detail of the `thiserror` crate")
+    (description
+      "Implementation detail of the `thiserror` crate")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-thiserror-1
+  (package
+    (name "rust-thiserror")
+    (version "1.0.50")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "thiserror" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "1ll2sfbrxks8jja161zh1pgm3yssr7aawdmaa2xmcwcsbh7j39zr"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:skip-build?
+        #t
+        #:cargo-inputs
+        (("rust-thiserror-impl" ,rust-thiserror-impl-1))))
+    (home-page
+      "https://github.com/dtolnay/thiserror")
+    (synopsis "derive(Error)")
+    (description "derive(Error)")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-edit-0.1
+  (package
+    (name "rust-edit")
+    (version "0.1.4")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (crate-uri "edit" version))
+        (file-name
+          (string-append name "-" version ".tar.gz"))
+        (sha256
+          (base32
+            "1d7v2yd2jckqr1v4rd1fgihqnad5wlxgkxbb9kg1ysdwyxqslqn5"))))
+    (build-system cargo-build-system)
+    (arguments
+      `(#:skip-build?
+        #t
+        #:cargo-inputs
+        (("rust-shell-words" ,rust-shell-words-1)
+         ("rust-tempfile" ,rust-tempfile-3)
+         ("rust-which" ,rust-which-4))))
+    (home-page
+      "https://github.com/milkey-mouse/edit")
+    (synopsis
+      "Open a file in the default text editor")
+    (description
+      "Open a file in the default text editor")
+    (license license:cc0)))
+
 (define-public uesave
   (package
     (name "uesave")
