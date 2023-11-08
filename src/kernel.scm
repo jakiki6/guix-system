b8645c6014b92b4cf8ea3526e50b22da0495beab
Jakob Kirsch <jakob.kirsch@web.de>
2023-11-08 20:21:32 +0100

    bump linux

--- src/kernel.scm
+++ src/kernel.scm
@@ -8,7 +8,7 @@
   (package
     (inherit linux)
     (name "linux-zen")
-    (version "6.6-zen1")
+    (version "6.6.1-zen1")
     (source
       (origin
         (method git-fetch)
@@ -18,4 +18,4 @@
         (file-name (git-file-name name version))
         (sha256
           (base32
-            "0rda54h5lg2llbwkj2h4mqfshjyha1dzlcwhx099is7g2lfzksxx"))))))
+            "13m820wggf6pkp351w06mdn2lfcwbn08ydwksyxilqb88vmr0lpq"))))))
