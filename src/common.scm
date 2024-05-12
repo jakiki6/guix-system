c79c7d7635857d06ce76c911ad30d94d7957894d
Jakob Kirsch <jakob.kirsch@web.de>
2024-05-12 03:23:08 +0200

    add fonts

--- src/common.scm
+++ src/common.scm
@@ -64,6 +64,8 @@
   (gnu packages crates-windows)
   (gnu packages crates-crypto)
   (gnu packages crates-tls)
+  (gnu packages fonts)
+  (gnu packages xorg)
   (gnu system setuid)
   (gnu system image)
   (gnu image)
@@ -273,7 +275,84 @@
                 distrobox-bumped
                 vim-as-vi
                 shutdown-as-poweroff
-                python3-as-python))))
+                python3-as-python)
+          (list font-adobe-source-code-pro
+                font-adobe-source-han-sans
+                font-adobe-source-sans-pro
+                font-adobe-source-serif-pro
+                font-anonymous-pro
+                font-anonymous-pro-minus
+                font-awesome
+                font-bitstream-vera
+                font-blackfoundry-inria
+                font-cns11643
+                font-cns11643-swjz
+                font-comic-neue
+                font-culmus
+                font-dejavu
+                font-dosis
+                font-dseg
+                font-fantasque-sans
+                font-fira-code
+                font-fira-mono
+                font-fira-sans
+                font-fontna-yasashisa-antique
+                font-gnu-freefont
+                font-gnu-unifont
+                font-go
+                font-google-material-design-icons
+                font-google-noto
+                font-google-roboto
+                font-hack
+                font-hermit
+                font-ibm-plex
+                font-inconsolata
+                font-iosevka
+                font-iosevka-aile
+                font-iosevka-etoile
+                font-iosevka-slab
+                font-iosevka-term
+                font-iosevka-term-slab
+                font-ipa-mj-mincho
+                font-jetbrains-mono
+                font-lato
+                font-liberation
+                font-linuxlibertine
+                font-lohit
+                font-meera-inimai
+                font-mononoki
+                font-mplus-testflight
+                font-opendyslexic
+                font-public-sans
+                font-rachana
+                font-sarasa-gothic
+                font-sil-andika
+                font-sil-charis
+                font-sil-gentium
+                font-tamzen
+                font-terminus
+                font-tex-gyre
+                font-un
+                font-vazir
+                font-wqy-microhei
+                font-wqy-zenhei
+                font-adobe100dpi
+                font-adobe75dpi
+                font-cronyx-cyrillic
+                font-dec-misc
+                font-isas-misc
+                font-micro-misc
+                font-misc-cyrillic
+                font-misc-ethiopic
+                font-misc-misc
+                font-mutt-misc
+                font-schumacher-misc
+                font-screen-cyrillic
+                font-sony-misc
+                font-sun-misc
+                font-util
+                font-winitzki-cyrillic
+                font-xfree86-type1))))
     (services
       (remove
         (lambda (service)
