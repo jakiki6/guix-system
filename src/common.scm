694aa17ee68f02c5ea14aa484c20acbf4a5159b4
Jakob Kirsch <jakob.kirsch@web.de>
2024-05-14 00:00:50 +0200

    Revert "remove dockerd"
    This reverts commit d7715728c9302df771c29edda226cb884abbe6da.

--- src/common.scm
+++ src/common.scm
@@ -372,6 +372,19 @@
                   guix-publish-service-type
                   (guix-publish-configuration (advertise? #t)))
                 (service gnome-keyring-service-type)
+                (service docker-service-type)
+                (service
+                  oci-container-service-type
+                  (list (oci-container-configuration
+                          (image "ipfs/kubo:latest")
+                          (provision "ipfs")
+                          (ports (list "4001:4001"
+                                       "4001:4001/udp"
+                                       "127.0.0.1:8080:8080"
+                                       "127.0.0.1:5001:5001"))
+                          (volumes
+                            (list "/ipfs_data:/data/ipfs"
+                                  "/ipfs_export:/export")))))
                 (service
                   mpd-service-type
                   (mpd-configuration
