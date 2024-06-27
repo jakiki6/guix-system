0f2ee1a1a9c9bc4a8880961b127e5a4806d1d361
Jakob Kirsch <jakob.kirsch@web.de>
2024-06-27 20:42:18 +0200

    bump shepherd

--- src/packages.scm
+++ src/packages.scm
@@ -8,7 +8,7 @@
         (uri (origin-uri (package-source shepherd-0.10)))
         (sha256
           (base32
-            "0206r2l914qjahzd1qill57r1qcg1x8faj0f6qv3x42wqx6x28ky"))
+            "0k40n9qm5r5rqf94isa1857ghd4329zc5rjf3ll2572gpiw3ij4x"))
         (patches
           (list (local-file
                   "../patches/shepherd-reboot-kexec.patch")))))))
