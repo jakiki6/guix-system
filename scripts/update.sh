ddd2cee0adb06d43643b3b092d30fda06ffacc28
Jakob Kirsch <jakob.kirsch@web.de>
2024-09-04 00:13:38 +0200

    ekdaskpadskpdsapk[das

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,4 +2,4 @@
 
 until sudo $(which guix) system reconfigure configs/$(hostname).scm --allow-downgrades; do sleep 5; done
 sudo python3 scripts/copy_kernel.py
-until guix upgrade; do sleep 5; done
+until guix home reconfigure ./home-configuration.scm; do sleep 5; done
