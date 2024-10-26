b65f8ebb210ec7b5d1e330c900a47e5f1dd85719
Jakob Kirsch <jakob.kirsch@web.de>
2024-10-26 22:50:05 +0200

    fix guix-science url

--- home-configuration.scm
+++ home-configuration.scm
@@ -535,7 +535,7 @@
                      (branch "master"))
                    (channel
                      (name 'guix-science)
-                     (url "https://codeberg.org/guix-science/guix-science")
+                     (url "https://codeberg.org/guix-science/guix-science.git")
                      (introduction
                        (make-channel-introduction
                          "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
