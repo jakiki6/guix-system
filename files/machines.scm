60c48bace4f477e0f942c9ce29e2889eb06e40d5
Jakob Kirsch <jakob.kirsch@web.de>
2024-12-25 20:54:50 +0100

    add machines.scm

---
+++ files/machines.scm
@@ -0,0 +1,6 @@
+(list (build-machine
+        (name "192.168.69.73")
+        (systems (list "x86_64-linux" "i686-linux"))
+        (host-key
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKHtoADGArfIQ0Hhr1R4KljOeSGGzTpgF+qobzsJhmV")
+        (user "root")))
