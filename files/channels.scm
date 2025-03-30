58443383901c9dde85faf3c5c93bdaccf0522d53
Jakob Kirsch <jakob.kirsch@web.de>
2025-03-30 22:55:25 +0200

    it was merged yay

--- files/channels.scm
+++ files/channels.scm
@@ -11,7 +11,6 @@
                 (77103 0)
                 (77262 0)
                 (77293 0)
-                (77294 1)
                 (77343 0))))
           (introduction
             (make-channel-introduction
